a023a1d307ad206f305f58670cc104d787ce0e34
Safy1993 <53688212+Safy1993@users.noreply.github.com>
2019-09-16 14:27:53 +0400

    Day 1

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BallControl : MonoBehaviour
 {
+    public float zForce = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +14,20 @@ public class BallControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("z"))
+        {
+            zForce += 100; 
+        }
+
+        if (Input.GetKeyDown("x"))
+        {
+            zForce += 10;
+        }
     }
 
     void OnMouseDown()
     {
-        GetComponent<Rigidbody>().AddRelativeForce(0,0, 500);
+        GetComponent<Rigidbody>().AddRelativeForce(0,0, zForce);
 
     }
 }
