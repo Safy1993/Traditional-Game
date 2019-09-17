46bd01e1cd9e276efe5479fe7342747a35c96e13
radiyah2014 <53684194+radiyah2014@users.noreply.github.com>
2019-09-17 13:03:43 +0400

    Main menue

---
+++ Assets/Scenes/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Toggle audioToggle;
+
+    void Start()
+    {
+        int mute = PlayerPrefs.GetInt("Mute", 0);
+        audioToggle.isOn = mute == 1;
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+    public void MuteSounds()
+    {
+        if (audioToggle.isOn)
+            PlayerPrefs.SetInt("Mute", 1);
+        else
+            PlayerPrefs.SetInt("Mute", 0);
+
+    }
+}
