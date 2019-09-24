289410b87e30580d2fb943ed5b8a1e76a9bed2c0
radiyah2014 <53684194+radiyah2014@users.noreply.github.com>
2019-09-24 15:43:31 +0400

    add ball animation

---
+++ Assets/Scenes/Knock it Down/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject ball;
+    Plane plane = new Plane(Vector3.forward,0);
+    public Transform Target;
+    public float ballForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = Target.position - ball.transform.position;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //shoot the ball
+            ball.GetComponent<Rigidbody>().AddForce(dir* ballForce, ForceMode.Impulse);
+        }
+        float dist;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(plane.Raycast (ray,out dist))
+        {
+            Vector3 point = ray.GetPoint(dist);
+            Target.position = new Vector3(point.x, point.y, 0);
+        }
+        
+    }
+}
