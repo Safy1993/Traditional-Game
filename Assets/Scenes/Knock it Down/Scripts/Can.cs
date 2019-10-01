294b88a00ef25861279ffd32cd7cd679c7ae8131
radiyah2014 <53684194+radiyah2014@users.noreply.github.com>
2019-10-01 11:54:21 +0400

    BombCan

--- Assets/Scenes/Knock it Down/Scripts/Can.cs
+++ Assets/Scenes/Knock it Down/Scripts/Can.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Can : MonoBehaviour
 {
    public bool hasFallen;
+   public bool isBombCan;
+    private int blastForce=1000;
+    private int blastRaduis = 20;
+    //public ParticleSystem MuzzuleFlash;
+    //MuzzuleFlash.Play();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,5 +32,25 @@ public class Can : MonoBehaviour
             UIManager.instance.UpdateScore();
         }
 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isBombCan)
+        {
+            if (collision.gameObject.name=="Ball")
+            {
+                Collider[] colliders = Physics.OverlapSphere(transform.position,blastRaduis);
+                foreach (Collider c in colliders)
+                {
+                    Rigidbody rb = c.GetComponent<Rigidbody>();
+                    if (rb != null)
+                    {
+                        rb.AddExplosionForce(blastForce, transform.position, blastRaduis, 4, ForceMode.Impulse);
+                    }
+
+                }
+            }
+         
+        }
     }
 }
